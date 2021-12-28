202cf714dde8c51272ca6bd5fec2a3c3b00ed9fc
Dampfwalze <paulsuckow03@gmail.com>
2021-12-28 16:15:20 +0100

    removed make

--- winmkdir.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-for %%x in (%*) do (
-    if not exist "%%~x" (
-        ::echo Create dir: %%~x
-        mkdir "%%~x"
-    )
-)
--- winrmdir.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-for %%x in (%*) do (
-    if exist "%%~x" (
-        ::echo Delete dir: %%~x
-        rmdir /s /q "%%~x"
-    )
-)
